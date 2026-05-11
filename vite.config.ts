9260d7ba12792dde6c760652b9498c4976cf4a41
Lawrence Owen <lawrence.owen@terria.io>
2026-05-11 12:37:26 +1000

    dist->docs

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "docs",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
